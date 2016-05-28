381db3198387d6c8682e50a05e7d856b2f770bb4
Filippo La Torre <filippo.latorre@stentle.com>
2016-05-28 19:43:05 +0200

    Fix

--- src/app.js
+++ src/app.js
@@ -27,24 +27,13 @@ function processEvent(event) {
         if (!sessionIds.has(sender)) {
             sessionIds.set(sender, uuid.v1());
         }
-var userName;
-            if(event.sender.id){
-userName = event.sender.id;
-            }
 
         console.log("Text", text);
+
         let apiaiRequest = apiAiService.textRequest(text,
-{
-sessionId: sessionIds.get(sender),
-contexts: [
-{
-name: "generic",
-parameters: {
-facebook_user: userName
-}
-}
-]
-});
+            {
+                sessionId: sessionIds.get(sender)
+            });
 
         apiaiRequest.on('response', (response) => {
             if (isDefined(response.result)) {
